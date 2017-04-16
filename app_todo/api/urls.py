58a2bb08f4b551a182f23816d7a014350a9715bf
viach <vch.380@gmail.com>
2017-04-16 23:54:43 +0300

    alpha done

--- app_todo/api/urls.py
+++ app_todo/api/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
-from .views import TaskListView
+from .views import TaskListView, TaskUpdateView, TaskDeleteView, TaskCreateView
 
 urlpatterns = [
     url(r'^$', TaskListView.as_view()),
+    url(r'^update/$', TaskUpdateView.as_view()),
+    url(r'^create/$', TaskCreateView.as_view()),
+    url(r'^delete/(?P<id>\d+)$', TaskDeleteView.as_view()),
 ]
--- app_todo/api/views.py
+++ app_todo/api/views.py
@@ -1,12 +1,35 @@
-from rest_framework.generics import ListAPIView
-
+from rest_framework.generics import ListAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView
 from .serializers import TaskSerializer
 from app_todo.models import Task
 
 
 class TaskListView(ListAPIView):
-    serializer_class = TaskSerializer
     model = Task
+    serializer_class = TaskSerializer    
 
     def get_queryset(self):
         return Task.objects.all()
+
+
+class TaskUpdateView(UpdateAPIView):    
+    model = Task
+    serializer_class = TaskSerializer
+    
+    def get_object(self, *args, **kwargs):        
+        return Task.objects.get(pk=self.request.data['id'])
+
+
+class TaskDeleteView(DestroyAPIView):
+    model = Task
+    serializer_class = TaskSerializer
+    
+    def get_object(self, *args, **kwargs):           
+        return Task.objects.get(pk=self.kwargs['id'])
+
+
+class TaskCreateView(CreateAPIView):
+    model = Task
+    serializer_class = TaskSerializer
+    
+    def get_object(self, *args, **kwargs):        
+        return Task.objects.get(pk=self.request.data['id'])
